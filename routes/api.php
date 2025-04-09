0ef5bf6d81c4ac76c02014649f180f2b554f1613
ArielOrellana <orellanaariel1998@gmail.com>
2025-04-08 23:10:08 -0300

    prueba tecnica

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\SocialiteController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +28,5 @@ Route::middleware('auth:api')->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
     Route::apiResource('tasks', TaskController::class);
+    Route::get('users', [UserController::class, 'index']);
 });
